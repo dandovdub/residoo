ae05daa9b15cfc9bb3a50be6fb6c30d9183ac288
Dan <323850340+dandovdub@users.noreply.github.com>
2026-09-04 09:42:01 -0400

    release: sync Homebrew formula sha256 to the real published 0.8.4 tarball

--- packaging/homebrew/residoo.rb
+++ packaging/homebrew/residoo.rb
@@ -8,7 +8,7 @@ class Residoo < Formula
   homepage "https://github.com/dandovdub/residoo"
   url "https://registry.npmjs.org/residoo/-/residoo-0.8.4.tgz"
   # sha256 of the real published tarball: curl -sL <url> | shasum -a 256
-  sha256 "6748c2e27ce0ff6d1cdc96a155f93a808083481abd1e759791ea1742b0dc8acb"
+  sha256 "4830a5cc8c2e33f1a37ae0b6d17a774adeebbe049d94ffdaeceea209b204d74d"
   license "MIT"
 
   depends_on "node"
